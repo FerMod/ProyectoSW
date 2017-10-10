54569376dc325daec158ef01241dff462383aca2
FosterGun <fostergun123@gmail.com>
2017-10-10 23:30:11 +0200

    Esto anda (#1) (Tendrá que hacerlo)
    * Fixed JQuery error
    
    Missing 'function()' and the opening and closing brakets
    Code indentation changed for better a better reading
    
    * Added style changes to 'quizes.html'
    
    Minor style changes that does not affect much overall
    
    * Added new lines to files
    
    * Changed files indentation
    
    Only indentation changes to make things more readable
    
    * Code separated from html file
    The events are treated now
    
    * Excluded
    
    * Added '.gitignore' file
    
    * Excluded
    
    * Renamed folder and fixed some ixues
    
    * Removed unnecessary returns
    
    * Removed a condition neggation
    
    I dont know who did that >.>
    
    * Renamed foolder from 'estilo' to 'css'
    
    * Implemented php to html
    
    * Changed the references to the css files and the quiz script
    
    * Removed old files
    
    * Okay... now yeah... i think... :,>

---
+++ js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	$("#fpreguntas").on("submit", function() {
+
+		var emailcomp = new RegExp('/^[a-zA-Z]+\d{3}@ikasle\.ehu\.(eus|es)$/');
+
+		var email = $("#email").val();
+		var enunciado = $("#enunciado").val();
+		var respuestacor = $("#respuestacorrecta").val();
+		var respuestaincor = $("#respuestaincorrecta").val();
+		var respuestaincor1 = $("#respuestaincorrecta1").val();
+		var respuestaincor2 = $("#respuestaincorrecta2").val();
+		var com = $("#complejidad").val();
+		var tema = $("#tema").val();
+
+		if(email != "" && enunciado != "" && respuestacor != "" && respuestaincor != "" && respuestaincor1 != "" && respuestaincor2 != "" && com != "" && tema != "") {
+			if(emailcomp.test(email)) {
+				if(isNan(com)) {
+					if(parseInt(com) >= 1 && parseInt(com) <= 5) {
+						//Envía datos
+						return true;
+					} else {
+						alert("El número de complejidad debe situarse entre 1 y 5.");
+					}
+				} else {
+					alert("El campo de complejidad debe tener un número.");
+				}
+			} else {
+				alert("La dirección de email introducida no es válida.");
+			}
+		} else {
+			alert("No debe dejar ningún campo vacío.");
+		}
+		
+		return false;
+
+	});
+});

5ee6f24577cfbce0324da80d76282a7d64e764d9
FerMod <tudelaigarcia@gmail.com>
2017-10-10 22:28:58 +0200

    Changed the references to the css files and the quiz script

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 		var tema = $("#tema").val();
 
 		if(email != "" && enunciado != "" && respuestacor != "" && respuestaincor != "" && respuestaincor1 != "" && respuestaincor2 != "" && com != "" && tema != "") {
-			if(emailcomp.test(email)) {
+			if(!emailcomp.test(email)) {
 				if(parseInt(com) >= 1 && parseInt(com) <= 5) {
 					//Envía datos
 					return true;

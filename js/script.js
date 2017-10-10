1aa37f50b12ae335be980e548179fe01f1727055
FosterGun <fostergun123@gmail.com>
2017-10-10 23:24:37 +0200

    Okay... now yeah... i think... :,>

--- js/script.js
+++ js/script.js
@@ -13,12 +13,16 @@ $(document).ready(function() {
 		var tema = $("#tema").val();
 
 		if(email != "" && enunciado != "" && respuestacor != "" && respuestaincor != "" && respuestaincor1 != "" && respuestaincor2 != "" && com != "" && tema != "") {
-			if(!emailcomp.test(email)) {
-				if(parseInt(com) >= 1 && parseInt(com) <= 5) {
-					//Envía datos
-					return true;
+			if(emailcomp.test(email)) {
+				if(isNan(com)) {
+					if(parseInt(com) >= 1 && parseInt(com) <= 5) {
+						//Envía datos
+						return true;
+					} else {
+						alert("El número de complejidad debe situarse entre 1 y 5.");
+					}
 				} else {
-					alert("El número de complejidad debe situarse entre 1 y 5.");
+					alert("El campo de complejidad debe tener un número.");
 				}
 			} else {
 				alert("La dirección de email introducida no es válida.");

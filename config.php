0ec905bf89d2a161eae6fce77a02dfce70086f60
FerMod <tudelaigarcia@gmail.com>
2017-10-16 19:47:40 +0200

    Added database config file
    Updated the file 'quizes.html' to use the config file. (Still in WIP)

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+$local = false;
+
+if($local) {
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "quiz";
+} else {
+	$servername = "";
+	$username = "";
+	$password = "";
+	$database = "";
+}
+
+?>

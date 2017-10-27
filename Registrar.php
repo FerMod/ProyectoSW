865e5574fe6acbba30332a3b6326a12033915ee7
FerMod <tudelaigarcia@gmail.com>
2017-10-27 20:03:25 +0200

    Applied the new layout to 'Registrar.php'

--- Registrar.php
+++ Registrar.php
@@ -1,52 +1,54 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta name="tipo_contenido" content="text/html;" http-equiv="content-type" charset="utf-8">
-		<title>Preguntas</title>
-		<link rel='stylesheet' type='text/css' href='css/style.css' />
-		<link rel='stylesheet' 
-			type='text/css' 
-			media='only screen and (min-width: 530px) and (min-device-width: 481px)'
-			href='css/wide.css' />
-		<link rel='stylesheet' 
-			type='text/css' 
-			media='only screen and (max-width: 480px)'
-			href='css/smartphone.css' />
-	</head>
-	<body>
-		<div id='page-wrap'>
-			<header class='main' id='h1'>
-				<span class="right"><a href="registro">Registrarse</a></span>
-				<span class="right"><a href="login">Login</a></span>
-				<span class="right" style="display:none;"><a href="/logout">Logout</a></span>
-				<h2>Quiz: el juego de las preguntas</h2>
-			</header>
-			<nav class='main' id='n1' role='navigation'>
-				<span><a href='layout.html'>Inicio</a></span>
-				<span><a href='quizes.php'>Preguntas</a></span>
-				<span><a href='creditos.html'>Creditos</a></span>
-			</nav>
-			<section class="main" id="s1">
-					<form id="registro">	
-						<fieldset>
-						<legend>REGISTRO</legend>
-							<div>
-							<label>Escriba su email <strong><font size="3" color="red">*</font></strong></label>
-							<input type="text" name="email" size="35">
-							</div>
-						
-						
-							<div>
-							<label>Confirme su email <strong><font size="3" color="red">*</font></strong></label>
-							<input type="text" name="emailver" size="25">
-							</div>
-						</fieldset>
-					</form>
-			</section>
-			<footer class='main' id='f1'>
-				<p><a href="http://es.wikipedia.org/wiki/Quiz" target="_blank">¿Qué es un Quiz?</a></p>
-				<a href='https://github.com/FerMod/ProyectoSW'>Link GITHUB</a>
-			</footer>
-		</div>
-	</body>
+<head>
+	<meta name="tipo_contenido" content="text/html;" http-equiv="content-type" charset="utf-8">
+	<title>Preguntas</title>
+
+	<script src="https://code.jquery.com/jquery-3.2.1.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+	<script src="js/script.js"></script>	
+
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+	<header>
+		<span ><a href="registro">Registrarse</a></span>
+		<span><a href="login">Login</a></span>
+		<span style="display:none;"><a href="/logout">Logout</a></span>
+		<h2>Quiz: el juego de las preguntas</h2>
+	</header>
+	<div class="container">
+		<nav class="navbar" role="navigation">
+			<span><a href='layout.html'>Inicio</a></span>
+			<span><a href='quizes.php'>Preguntas</a></span>
+			<span><a href='creditos.html'>Creditos</a></span>
+		</nav>
+		<article class="content">
+			<form id="registro">	
+				<fieldset>
+					<legend>REGISTRO</legend>
+					<div>
+						<label>Escriba su email <strong><font size="3" color="red">*</font></strong></label>
+						<input type="text" name="email" size="35">
+					</div>
+					
+					
+					<div>
+						<label>Confirme su email <strong><font size="3" color="red">*</font></strong></label>
+						<input type="text" name="emailver" size="25">
+					</div>
+				</fieldset>
+			</form>
+		</article>		
+		<aside class="sidebar">
+			Sidebar contents<br/>(sidebar)
+		</aside>
+	</div>
+	<footer>
+		<p><a href="http://es.wikipedia.org/wiki/Quiz" target="_blank">¿Qué es un Quiz?</a></p>
+		<a href='https://github.com/FerMod/ProyectoSW'>Link GITHUB</a>
+	</footer>
+
+</body>
 </html>

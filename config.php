b5083a1ef0f59ed8ca35363cf38d4a1744f59edb
FerMod <tudelaigarcia@gmail.com>
2017-10-30 21:26:41 +0100

    Added the new variables to 'config.php'
    Added the variable '', that contains the user profile images.
    
    Automated the creation of the user profile image folder.

--- config.php
+++ config.php
@@ -15,9 +15,14 @@ if($local) {
 }
 
 $imageUploadFolder = './img/uploads/';
+$profileImageFolder = './img/users/';
 
 if (!file_exists($imageUploadFolder)) {
 	mkdir($imageUploadFolder, 0777, true);
+}
+
+if (!file_exists($profileImageFolder)) {
+	mkdir($profileImageFolder, 0777, true);
 }
 
 ?>

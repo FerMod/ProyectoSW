565ec6d6f42700aa060721bc2d0cc81ab8e2a9b1
FerMod <tudelaigarcia@gmail.com>
2017-11-04 12:39:17 +0100

    Updated the xml file path
    The file path is taken from the variable '' declared in config.php

--- VerPreguntasXML.php
+++ VerPreguntasXML.php
@@ -1,9 +1,14 @@
 <?php
-	$xslDoc = new DOMDocument();
-	$xslDoc->load("preguntas.xsl");
-	$xmlDoc = new DOMDocument();
-	$xmlDoc->load("preguntas.xml");
-	$proc = new XSLTProcessor();
-	$proc->importStylesheet($xslDoc);
-	echo $proc->transformToXML($xmlDoc);
+
+$xslDoc = new DOMDocument();
+$xslDoc->load($xmlFolder . "preguntas.xsl");
+
+$xmlDoc = new DOMDocument();
+$xmlDoc->load($xmlFolder . "preguntas.xml");
+
+$proc = new XSLTProcessor();
+$proc->importStylesheet($xslDoc);
+
+echo $proc->transformToXML($xmlDoc);
+
 ?>

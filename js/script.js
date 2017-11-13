9f4e00c996fb983aa50ce580985c13c2ce8bba1e
FosterGun <fostergun123@gmail.com>
2017-11-13 21:04:09 +0100

    getQuestionsStats pending fix

--- js/script.js
+++ js/script.js
@@ -149,7 +149,7 @@ $(document).ready(function() {
 			type: "post",                // Type of request to be send, called as method
 			data: formData,                // Data sent to server, a set of key/value pairs (i.e. form fields and values)
 			contentType: false,            // The content type used when sending data to the server.
-			dataType: "json",
+			dataType: "json",				//Data type is JSON
 			cache: false,                // To unable request pages to be cached
 			processData:false,            // To send DOMDocument or non processed data file it is set to false
 			success: function(result, status, xhr) {

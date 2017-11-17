72c11fad38f3743d0f842a0471dfe789db5b33a8
FerMod <tudelaigarcia@gmail.com>
2017-11-17 14:00:52 +0100

    Add NuSOAP library

--- Registrar.php
+++ Registrar.php
@@ -43,6 +43,32 @@
 					$dataCorrect = false;
 					$dataCheckMessage .= "<div class=\"serverMessage\" id=\"serverDefaultMessage\">El formato del email no es correcto.<br>Debe cumplir el formato de la UPV/EHU.</div>";
 				}
+
+				// TODO:
+
+				/*
+				//incluimos la clase nusoap.php
+				require_once('../lib/nusoap.php');
+				require_once('../lib/class.wsdlcache.php');
+
+				//creamos el objeto de tipo soapclient.
+				//http://www.mydomain.com/server.php se refiere a la url
+				//donde se encuentra el servicio SOAP que vamos a utilizar.
+				$soapclient = new nusoap_client( 'http://footballpool.dataaccess.eu/data/info.wso?Wsdl',true);
+
+				//Llamamos la función que habíamos implementado en el Web Service
+				//e imprimimos lo que nos devuelve
+				$result = $soapclient->call('YellowAndRedCardsTotal');
+				print_r($result);
+				
+				echo '<br>';
+				echo '<b>EUROCOPA 2016:</b> <br>';
+				echo 'Tarjetas Amarillas:' . $result['YellowAndRedCardsTotalResult']['iYellow'];
+				echo '<br>';
+				echo 'Tarjetas Rojas:' . $result['YellowAndRedCardsTotalResult']['iRed'];
+				*/
+
+
 				if (existsEmail($email, $conn)) {
 					$dataCorrect = false;
 					$dataCheckMessage .= "<div class=\"serverMessage\" id=\"serverErrorMessage\">Ya existe una cuenta con el email introducido.</div>";

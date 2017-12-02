c4360d82a33df218f709a2d965d4490509061967
FerMod <tudelaigarcia@gmail.com>
2017-12-02 18:22:18 +0100

    Add php file used to get the navbar items that will show to the user

---
+++ navbar_items.php
@@ -0,0 +1,27 @@
+<?php 
+
+echo '<span><a href="layout.php">Inicio</a></span>';
+
+if((isset($_SESSION['logged_user']) && !empty($_SESSION['logged_user'])) && (isset($_SESSION['user_type']) && !empty($_SESSION['user_type']))) {
+
+	$userType = $_SESSION['user_type'] ?? '';				
+
+	switch ($userType) {
+		case 'teacher':
+		echo '<span><a href="RevisarPreguntas.php">Revisar preguntas</a></span>';
+		break;
+
+		case 'student':
+		echo '<span><a href="quizes.php">Hacer pregunta</a></span>';
+		break;
+	}
+
+
+	echo '<span><a href="VerPreguntasConFoto.php">Ver preguntas</a></span>';
+	echo '<span><a href="GestionPreguntas.php">Gestionar preguntas</a></span>';
+
+}
+
+echo '<span><a href="creditos.php">Creditos</a></span>';
+
+?>

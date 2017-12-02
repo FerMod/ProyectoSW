b985b96de376257e87e9a9cc945706c4a321ba5a
FerMod <tudelaigarcia@gmail.com>
2017-12-02 15:44:51 +0100

    Fix session id regeneration

--- session_timeout.php
+++ session_timeout.php
@@ -1,37 +1,56 @@
 <?php
 
+include('DebugTools/ChromePhp.php');
+
+$config = include("config.php");
+
 // if (!function_exists('refreshSessionTimeout')) {
 
 function refreshSessionTimeout() {
-
-	$config = include("config.php");
 
 	if (isSessionTimedout()) {
 		// last request was more than the 'timeout' time value ago
 		session_unset();
 		session_destroy();
 	}
-	$_SESSION['last_activity'] = time(); // update last activity time stamp
 
-	if (!isset($_SESSION['creation_time'])) {
-		$_SESSION['creation_time'] = time();
-	} else if (time() - $_SESSION['creation_time'] > $config["session"]["timeout"]) {
-		// session started more than the 'timeout' time ago		
-		if (!headers_sent()) {
-			session_regenerate_id(true);// change session ID for the current session and invalidate old session ID
-		} else {
-			@session_regenerate_id(true);
-		}
-		$_SESSION['creation_time'] = time(); // update creation time
-	}
+	refreshSessionCreationTime();
 
 }
 
 // }
 
 function isSessionTimedout() {
-	$config = include("config.php");
-	return isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $config["session"]["timeout"]);
+
+	ChromePhp::log($_SESSION);
+
+	global $config;
+
+	if(isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity'] > $config["session"]["timeout"])) {
+		return true;
+	}
+
+	$_SESSION['last_activity'] = time(); // update last activity time stamp
+
+	return false;
+}
+
+function refreshSessionCreationTime($delete_old_session = false) {
+
+	global $config;
+
+	if (!isset($_SESSION['creation_time'])) {
+		$_SESSION['creation_time'] = time();
+	} else if (time() - $_SESSION['creation_time'] > $config["session"]["timeout"]) {
+		// session started more than the 'timeout' time ago		
+		if (!headers_sent()) {
+			session_regenerate_id($delete_old_session);// change session ID for the current session and invalidate old session ID
+		} else {
+			@session_regenerate_id($delete_old_session);
+		}
+		$_SESSION['creation_time'] = time(); // update creation time
+	}
+
 }
 
 
